da2958fc04a658ae409bd3e1c99e118eb0164594
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-10 19:55:50 +0200

    Adds link to home.

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { WrapperProps } from "../interfaces";
 
 import Meta from "./Meta";
@@ -7,7 +8,16 @@ const Wrapper: React.FC<WrapperProps> = ({ children }): JSX.Element => {
 	return (
 		<>
 			<header>
-				<Image src="/logo.svg" alt="Trainerr Logo" height={100} width={100} />
+				<Link href="/">
+					<a>
+						<Image
+							src="/logo.svg"
+							alt="Trainerr Logo"
+							height={100}
+							width={100}
+						/>
+					</a>
+				</Link>
 			</header>
 			<Meta />
 			{children}
