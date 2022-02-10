b4855ffc311cc7eac63666e81ae001da39ad417b
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-10 19:54:35 +0200

    Adds logo in header.

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { WrapperProps } from "../interfaces";
 
 import Meta from "./Meta";
@@ -5,6 +6,9 @@ import Meta from "./Meta";
 const Wrapper: React.FC<WrapperProps> = ({ children }): JSX.Element => {
 	return (
 		<>
+			<header>
+				<Image src="/logo.svg" alt="Trainerr Logo" height={100} width={100} />
+			</header>
 			<Meta />
 			{children}
 		</>
