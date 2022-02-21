5d620c202c33a67411041fb2d35b2e1d4c198286
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-21 19:47:16 +0200

    Gets webpage via puppeteer.

--- pages/api/scrape/index.ts
+++ pages/api/scrape/index.ts
@@ -1,6 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import puppeteer from "puppeteer";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		const browser = await puppeteer.launch({
+			headless: true,
+			ignoreHTTPSErrors: true,
+		});
+		const page = await browser.newPage();
+		await page.goto("https://cttrains.co.za");
+		await page.screenshot({ path: "example.png" });
+		await browser.close();
+	} catch (error) {
+		return res.status(400).json({ success: false, error });
+	}
+
 	res.status(200).json({ message: "Hello there" });
 };
 
