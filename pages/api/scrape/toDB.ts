5b23c2f71133510b789301c42f3ecb4376409304
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-23 23:52:52 +0200

    Parses json data nicely.

--- pages/api/scrape/toDB.ts
+++ pages/api/scrape/toDB.ts
@@ -4,19 +4,31 @@ import fs from "fs";
 import Route from "../../../models/Route";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	let pairs: any[] = [];
 	try {
 		const data = JSON.parse(
-			fs.readFileSync("/output.json", {
+			fs.readFileSync("./output.json", {
 				encoding: "utf8",
 				flag: "r",
 			})
 		);
-		data.forEach((item: Object) => {
-			console.log(item);
+		let pair: any[] = [];
+		data.forEach((item: [Object]) => {
+			item.forEach((el: Object, index: number) => {
+				if (index % 2 === 0) {
+					pair.push(el);
+				} else {
+					pair.push(el);
+					pairs.push(pair);
+					pair = [];
+				}
+			});
 		});
 	} catch (error) {
 		return res.status(400).json({ success: false, error });
 	}
 
-	res.status(200).json({ success: true });
+	res.status(200).json({ success: true, pairs });
 };
+
+export default handler;
