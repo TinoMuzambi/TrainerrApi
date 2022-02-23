376ca43a6e73a06c326b07af4b9989a81fce19a4
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-23 21:23:57 +0200

    Cycle through intermediate routes.

--- pages/api/scrape/index.ts
+++ pages/api/scrape/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import puppeteer from "puppeteer";
+import fs from "fs";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	let browser;
@@ -16,64 +17,77 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 		const page = await browser.newPage();
 		await page.goto("https://cttrains.co.za/ss_route_select.php");
 
-		// Select depart station
-		await page.select("#departStation", "1");
-
-		// Select arrival station
-		await page.select("#arriveStation", "2");
-
-		let pageContent = {};
-
-		// Select travel days
-		await page.click(
-			"body > form > div:nth-child(5) > ul > li > div:nth-child(2) > input[type=Radio]"
+		// Get options length
+		const optionsLength = await page.$eval(
+			"#departStation",
+			(node) => node.children.length
 		);
+		let pageContent;
+		for (let i = 0; i < optionsLength; i++) {
+			pageContent = {};
 
-		// Select preferred time
-		await page.click("#entireCheck");
+			for (let j = i + 1; j < optionsLength; j++) {
+				// Select depart station
+				await page.select("#departStation", `${i + 1}`);
 
-		// Submit form and wait for page navigation
-		await Promise.all([
-			page.click("#time > ul > li > input[type=submit]"),
-			page.waitForNavigation({ waitUntil: "domcontentloaded" }),
-		]);
+				// Select arrival station
+				await page.select("#arriveStation", `${j + 1}`);
 
-		// Get page content.
-		const trainNumbers = await page.$$eval(
-			"body > .bgMiddle > table > tbody > tr > td",
-			(nodes) =>
-				nodes.map((node) => Number.parseInt(node.innerHTML.split(" ")[2]))
-		);
-		const departArriveTimes = await page.$$eval(
-			"body > .bgBottom > table > tbody > tr",
-			(nodes) => nodes.map((node) => node.innerHTML.replace("\n", ""))
-		);
-		const departArriveTimesObj = departArriveTimes.map((time, index) => {
-			let type: string = "";
-			let typeTime: string = "";
-			let city: string = "";
-			type = time.substring(time.indexOf(">") + 1, time.indexOf(":<"));
-			const strippedTime = time.substring(time.indexOf(type));
-			const cityTime = strippedTime.substring(
-				strippedTime.indexOf("color:black") + 13,
-				strippedTime.lastIndexOf("<")
-			);
+				// Select travel days
+				await page.click(
+					"body > form > div:nth-child(5) > ul > li > div:nth-child(2) > input[type=Radio]"
+				);
 
-			let cityTimeSplit = cityTime.split(" ").filter((el) => el !== "");
+				// Select preferred time
+				await page.click("#entireCheck");
 
-			typeTime = cityTimeSplit[cityTimeSplit.length - 1];
-			cityTimeSplit.length = cityTimeSplit.length - 1;
-			city = cityTimeSplit.join(" ");
+				// Submit form and wait for page navigation
+				await Promise.all([
+					page.click("#time > ul > li > input[type=submit]"),
+					page.waitForNavigation({ waitUntil: "domcontentloaded" }),
+				]);
 
-			return { type, city, typeTime };
-		});
+				// Get page content.
+				const trainNumbers = await page.$$eval(
+					"body > .bgMiddle > table > tbody > tr > td",
+					(nodes) =>
+						nodes.map((node) => Number.parseInt(node.innerHTML.split(" ")[2]))
+				);
+				const departArriveTimes = await page.$$eval(
+					"body > .bgBottom > table > tbody > tr",
+					(nodes) => nodes.map((node) => node.innerHTML.replace("\n", ""))
+				);
+				const departArriveTimesObj = departArriveTimes.map((time) => {
+					let type: string = "";
+					let typeTime: string = "";
+					let city: string = "";
+					type = time.substring(time.indexOf(">") + 1, time.indexOf(":<"));
+					const strippedTime = time.substring(time.indexOf(type));
+					const cityTime = strippedTime.substring(
+						strippedTime.indexOf("color:black") + 13,
+						strippedTime.lastIndexOf("<")
+					);
 
-		pageContent = departArriveTimesObj.map((obj, index) => {
-			return {
-				...obj,
-				trainNumber: trainNumbers[index],
-			};
-		});
+					let cityTimeSplit = cityTime.split(" ").filter((el) => el !== "");
+
+					typeTime = cityTimeSplit[cityTimeSplit.length - 1];
+					cityTimeSplit.length = cityTimeSplit.length - 1;
+					city = cityTimeSplit.join(" ");
+
+					return { type, city, typeTime };
+				});
+
+				pageContent = departArriveTimesObj.map((obj, index) => {
+					return {
+						...obj,
+						trainNumber: trainNumbers[Math.floor(index / 2)],
+					};
+				});
+
+				await page.goto("https://cttrains.co.za/ss_route_select.php");
+			}
+		}
+
 		pages.push(pageContent);
 	} catch (error) {
 		return res.status(400).json({ success: false, error });
@@ -82,6 +96,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	}
 
 	res.status(200).json({ success: true, pages });
+	fs.writeFileSync("output.json", JSON.stringify(pages));
 };
 
 export default handler;
