97af66877b06abdeda68f5d4632bf5abdb418d0f
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-23 23:31:26 +0200

    Adds route mongoose model.

---
+++ interfaces/models/Route.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+import { RouteModel } from "../../interfaces";
+
+const Route: Schema = new mongoose.Schema(
+	{
+		departingStation: {
+			type: String,
+			required: [true, "Route needs a departing station"],
+		},
+		arrivingStation: {
+			type: String,
+			required: [true, "Route needs an arriving station"],
+		},
+		departingTime: {
+			type: String,
+			required: [true, "Route needs a departing time"],
+		},
+		arrivingTime: {
+			type: String,
+			required: [true, "Route needs an arriving time"],
+		},
+		trainNumber: {
+			type: Number,
+			required: [true, "Route needs a train number"],
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export default mongoose.models.Route ||
+	mongoose.model<RouteModel>("Route", Route, "routes");
