0c2fb5bbb8453902867234da57627a2f28dc4945
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-14 19:54:42 +0200

    Adds pagination.

--- pages/api/routes/index.ts
+++ pages/api/routes/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import Route from "../../../models/Route";
+import { PAGE, PER_PAGE } from "../../../utils";
 import dbConnect from "../../../utils/db";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -9,9 +10,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	switch (method) {
 		case "GET": {
 			const line = req.query.line || "";
+			const page = Number.parseInt(req.query.page as string) || PAGE;
+			const perPage = Number.parseInt(req.query.perPage as string) || PER_PAGE;
+			const pages = Math.floor(471 / perPage);
+
 			try {
-				const routes = await Route.find(line ? { line: line } : {});
-				res.status(200).json({ success: true, routes });
+				const routes = await Route.find(line ? { line: line } : {})
+					.limit(perPage)
+					.skip(perPage * page);
+				res.status(200).json({ success: true, page, routes, pages });
 			} catch (error) {
 				res.status(400).json({ success: false, error });
 			}
