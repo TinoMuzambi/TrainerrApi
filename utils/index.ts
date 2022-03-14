90fbb396e5c582285abee51a351c14c2c05cf496
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-14 20:10:17 +0200

    Adds base url.

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,7 @@
 export const BASE_URL =
-	process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+	process.env.NODE_ENV === "production"
+		? "https://trainerr-api.vercel.app"
+		: "http://localhost:3000";
 
 export const PER_PAGE = 10;
 export const PAGE = 1;
